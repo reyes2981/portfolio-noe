6c561657878de7ced98953cc936b5f33df68774c
noe <noe.reyes92@yahoo.com>
2021-07-11 23:26:33 -0500

    created about.js file

--- app/javascript/components/home.js
+++ app/javascript/components/home.js
@@ -3,7 +3,9 @@ import React from 'react'
 class Home extends React.Component {
     render () {
         return (
-            <div>This is the hompe page</div>
+            <div>This is the home page</div>
         )        
     }
 }
+
+export default Home
